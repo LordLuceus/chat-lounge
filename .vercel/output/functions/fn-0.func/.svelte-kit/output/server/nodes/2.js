c7e8d2c6ef3f13627b58d175b025729519a5328f
LordLuceus <lord.luceus@gmail.com>
2024-02-25 20:18:28 +0100

    Delete vercel directory

--- .vercel/output/functions/fn-0.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CLaQjfeM.js","_app/immutable/chunks/scheduler.CYFynt7j.js","_app/immutable/chunks/index.iosNfj23.js","_app/immutable/chunks/index.D8mVmiTF.js"];
-export const stylesheets = ["_app/immutable/assets/2.C3rRPyVt.css"];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BSNCmJV_.js","_app/immutable/chunks/scheduler.CYFynt7j.js","_app/immutable/chunks/index.iosNfj23.js","_app/immutable/chunks/entry.BvPnGTHO.js","_app/immutable/chunks/index.D8mVmiTF.js"];
-export const stylesheets = [];
-export const fonts = [];

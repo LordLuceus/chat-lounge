815ef3b3966407e64a1fd9ea9237e8e73247d937
LordLuceus <lord.luceus@gmail.com>
2024-03-03 21:35:30 +0100

    Remove dotenv and use sveltekit env module

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,12 +2,11 @@ import { dev } from "$app/environment";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
 import ws from "ws";
+import { POSTGRES_PRISMA_URL } from "$env/static/private";
 
-dotenv.config();
 neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
+const connectionString = POSTGRES_PRISMA_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
 

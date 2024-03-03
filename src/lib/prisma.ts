f49485d175b741d812db5c7877334db2da8ec87a
LordLuceus <lord.luceus@gmail.com>
2024-03-03 22:02:23 +0100

    Use prisma edge client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,5 @@
-import { dev } from "$app/environment";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client/edge";
-import ws from "ws";
-import { POSTGRES_PRISMA_URL } from "$env/static/private";
 
-neonConfig.webSocketConstructor = ws;
-const connectionString = POSTGRES_PRISMA_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-
-const prisma = dev ? new PrismaClient() : new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;

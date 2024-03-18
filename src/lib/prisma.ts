3a8b7e53370acea40553f448ffbb7f7dc1065442
LordLuceus <lord.luceus@gmail.com>
2024-03-18 15:55:48 +0100

    Refactor Prisma initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,3 @@
-import { dev } from "$app/environment";
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { Pool, neonConfig } from "@neondatabase/serverless";
@@ -6,14 +5,9 @@ import ws from "ws";
 import { POSTGRES_PRISMA_URL } from "$env/static/private";
 
 neonConfig.webSocketConstructor = ws;
-let prisma: PrismaClient;
 
-if (dev) {
-  prisma = new PrismaClient();
-} else {
-  const neon = new Pool({ connectionString: POSTGRES_PRISMA_URL });
-  const adapter = new PrismaNeon(neon);
-  prisma = new PrismaClient({ adapter });
-}
+const neon = new Pool({ connectionString: POSTGRES_PRISMA_URL });
+const adapter = new PrismaNeon(neon);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;

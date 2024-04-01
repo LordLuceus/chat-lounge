2b5e5f7d6155630be3b85317cadd022bc46c595f
LordLuceus <lord.luceus@gmail.com>
2024-04-01 21:50:16 +0200

    Fix build error

--- src/lib/drizzle/migrate.ts
+++ src/lib/drizzle/migrate.ts
@@ -1,8 +1,16 @@
+import { createClient } from "@libsql/client";
 import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
 import { migrate } from "drizzle-orm/libsql/migrator";
-import { db } from "./db";
+import * as schema from "./schema";
 
 export const runMigrations = async () => {
+  const client = createClient({
+    url: process.env.TURSO_DB_URL!,
+    authToken: process.env.TURSO_DB_AUTH_TOKEN
+  });
+  const db = drizzle(client, { schema });
+
   console.log("Running migrations...");
   await migrate(db, { migrationsFolder: "src/lib/drizzle" });
   console.log("Migrations complete.");

f1b6069f0162540d34bcee4bceb63d1a9f8e5213
LordLuceus <lord.luceus@gmail.com>
2024-04-02 16:34:57 +0200

    Update migrate script to use JavaScript instead of TypeScript

--- src/lib/drizzle/migrate.js
+++ src/lib/drizzle/migrate.js
@@ -2,14 +2,14 @@ import { createClient } from "@libsql/client";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 import { migrate } from "drizzle-orm/libsql/migrator";
-import * as schema from "./schema";
 
 export const runMigrations = async () => {
   const client = createClient({
-    url: process.env.TURSO_DB_URL!,
+    // @ts-expect-error - TS complains about env vars not being defined
+    url: process.env.TURSO_DB_URL,
     authToken: process.env.TURSO_DB_AUTH_TOKEN
   });
-  const db = drizzle(client, { schema });
+  const db = drizzle(client);
 
   console.log("Running migrations...");
   await migrate(db, { migrationsFolder: "src/lib/drizzle" });

9f6d277afd9f1298e662edd16fe59a82ee163cc5
LordLuceus <lord.luceus@gmail.com>
2024-04-03 20:27:55 +0200

    Fix migration

--- src/lib/drizzle/0001_flaky_alex_wilder.sql
+++ src/lib/drizzle/0001_flaky_alex_wilder.sql
@@ -6,10 +6,3 @@ CREATE TABLE `model` (
 	`createdAt` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
 	`updatedAt` integer DEFAULT (CURRENT_TIMESTAMP) NOT NULL
 );
-
-INSERT INTO model (id, name, provider, tokenLimit) VALUES('mistral-large-latest','Mistral Large','mistral',32000);
-INSERT INTO model (id, name, provider, tokenLimit) VALUES('mistral-medium-latest','Mistral Medium','mistral',32000);
-INSERT INTO model (id, name, provider, tokenLimit) VALUES('mistral-small-latest','Mistral Small','mistral',32000);
-INSERT INTO model (id, name, provider, tokenLimit) VALUES('gpt-4-turbo-preview','GPT 4 Turbo','openai',128000);
-INSERT INTO model (id, name, provider, tokenLimit) VALUES('gpt-4','GPT 4','openai',8192);
-INSERT INTO model (id, name, provider, tokenLimit) VALUES('gpt-3.5-turbo','GPT 3.5 Turbo','openai',16385);

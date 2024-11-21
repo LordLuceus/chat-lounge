011f75543dab6a2ba792e7a5b5acdf3ab9304970
LordLuceus <lord.luceus@gmail.com>
2024-11-21 08:37:36 +0100

    Add embedded db replica

--- src/lib/drizzle/db.ts
+++ src/lib/drizzle/db.ts
@@ -4,7 +4,9 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
 const client = createClient({
-  url: TURSO_DB_URL,
-  authToken: TURSO_DB_AUTH_TOKEN
+  url: "file:data/chat-lounge.db",
+  syncUrl: TURSO_DB_URL,
+  authToken: TURSO_DB_AUTH_TOKEN,
+  syncInterval: 300
 });
 export const db = drizzle(client, { schema });

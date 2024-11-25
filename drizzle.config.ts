5262a1620118783c4fd8891df067fe276ba8ff69
LordLuceus <lord.luceus@gmail.com>
2024-11-25 12:28:06 +0100

    Use only local database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
-import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
   out: "./src/lib/drizzle",
   schema: "./src/lib/drizzle/schema.ts",
   dbCredentials: {
-    url: process.env.TURSO_DB_URL!,
-    authToken: process.env.TURSO_DB_AUTH_TOKEN!
+    url: "file:data/chat-lounge.db"
   },
   dialect: "turso",
   strict: true,
--- src/lib/drizzle/db.ts
+++ src/lib/drizzle/db.ts
@@ -1,12 +1,8 @@
-import { env } from "$env/dynamic/private";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
 const client = createClient({
-  url: "file:data/chat-lounge.db",
-  syncUrl: env.TURSO_DB_URL,
-  authToken: env.TURSO_DB_AUTH_TOKEN,
-  syncInterval: 300
+  url: "file:data/chat-lounge.db"
 });
 export const db = drizzle(client, { schema });

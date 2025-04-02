897cedcdbf352e3e9339f10a0f2550aa8a29f524
LordLuceus <lord.luceus@gmail.com>
2025-04-02 18:19:19 +0200

    Fix error on folder creation

--- src/routes/(app)/folders/+page.server.ts
+++ src/routes/(app)/folders/+page.server.ts
@@ -26,11 +26,11 @@ export const actions: Actions = {
     }
 
     const { name } = form.data;
-    const folderId = await createFolder({
+    const folder = await createFolder({
       userId: userId!,
       name
     });
 
-    return message(form, { folderId, created: true });
+    return message(form, { folderId: folder.id, created: true });
   }
 };

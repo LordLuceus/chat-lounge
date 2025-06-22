3a1e90e208002abc07dc6d4aa36814df5c794a1d
LordLuceus <lord.luceus@gmail.com>
2025-06-22 14:13:19 +0200

    Remove unnecessary sequence call from handle

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,7 @@
 import type { Handle } from "@sveltejs/kit";
-import { sequence } from "@sveltejs/kit/hooks";
 import { withClerkHandler } from "svelte-clerk/server";
 
-export const handle: Handle = sequence(
-  withClerkHandler({
-    signInUrl: "/auth/sign-in",
-    signUpUrl: "/auth/sign-up"
-  })
-);
+export const handle: Handle = withClerkHandler({
+  signInUrl: "/auth/sign-in",
+  signUpUrl: "/auth/sign-up"
+});
